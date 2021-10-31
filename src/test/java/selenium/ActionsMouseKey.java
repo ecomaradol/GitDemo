9abfc65becea0e6bc10cad882aee3686b3f8eb25
Zlata <ecomaradol@gmail.com>
2021-10-31 18:18:32 -0500

    all 10/31/2021 spaces

--- src/test/java/selenium/ActionsMouseKey.java
+++ src/test/java/selenium/ActionsMouseKey.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.interactions.Actions;
 
 public class ActionsMouseKey {
 
-
-
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C://Users/Zlata/Introduction/chromedriver.exe");
